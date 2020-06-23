17a58503e2a203ec37e1612e28dc7cc50b3c4173
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-06-23 20:25:16 +0100

    Chore: Composition on with PC

--- Composition/src/academy/learnprogramming/Main.java
+++ Composition/src/academy/learnprogramming/Main.java
@@ -3,6 +3,16 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Dimension dimension = new Dimension(20, 20, 5);
+        Case theCase = new Case("220B", "Dell", "240", dimension);
+        Resolution resolution = new Resolution(40, 40);
+        Monitor theMonitor = new Monitor("27 curved screen", "Dell", 27, resolution );
+
+        Motherboard theMotherboard = new Motherboard("Water cooled", "ASUS", 4, 2, "v2.44");
+
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+
+        thePC.getTheCase().powerButtonPress();
+        thePC.getMotherboard().loadProgram("Window 10");
     }
 }
