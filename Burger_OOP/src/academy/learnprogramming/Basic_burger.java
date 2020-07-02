e3bf4e18c733a0c8f81f00603b210e60486649bc
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-07-02 16:17:18 +0100

    Add: Burger OOP challenge

---
+++ Burger_OOP/src/academy/learnprogramming/Basic_burger.java
@@ -0,0 +1,32 @@
+package academy.learnprogramming;
+
+public class Basic_burger {
+
+    private String bunType;
+    private String meat;
+    private String addition_1;
+    private String addition_2;
+    private String addition_3;
+    private String addition_4;
+
+    public Basic_burger(String addition_1) {
+        this(addition_1, null, null, null);
+    }
+
+    public Basic_burger(String addition_1, String addition_2) {
+        this(addition_1, addition_2, null, null);
+    }
+
+    public Basic_burger(String addition_1, String addition_2, String addition_3) {
+        this(addition_1, addition_2, addition_3, null);
+    }
+
+    public Basic_burger(String addition_1, String addition_2, String addition_3, String addition_4) {
+        this.bunType = "Brioche";
+        this.meat = "Beef";
+        this.addition_1 = addition_1;
+        this.addition_2 = addition_2;
+        this.addition_3 = addition_3;
+        this.addition_4 = addition_4;
+    }
+}
