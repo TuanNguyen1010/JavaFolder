f13266815bd5d2f2f610de1412299e4cf64cbb72
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-07-10 10:50:11 +0100

    Chore: De-referencing arrays

--- referenceAndValue/src/academy/learnprogramming/Main.java
+++ referenceAndValue/src/academy/learnprogramming/Main.java
@@ -28,5 +28,15 @@ public class Main {
 
         System.out.println("myIntArray = " + Arrays.toString(myIntArray));
         System.out.println("anotherArray = " + Arrays.toString(anotherArray));
+
+        modifyArray(myIntArray);
+        anotherArray = new int[]{1,2,3,4,5};
+
+        System.out.println("After modify method myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("After modify method anotherArray = " + Arrays.toString(anotherArray));
+    }
+
+    private static void modifyArray(int[] array){
+        array[0] = 2;
     }
 }
